0c6838e7cce4f0de078bbfca5583dcc4ca8d40fa
Alexandre Florentin <lexfloren@gmail.com>
2015-04-06 16:38:15 +0200

    Essai de modélisation du plateau
    Recopie des techniques du sujet, essai de modélisation du plateau par le
    n-uplet (après avoir essayé de faire bool->bool->...)

---
+++ merlin.v
@@ -0,0 +1,9 @@
+Require Import Bool.
+Require Import List.
+Require Import NArith.
+Require Import Omega.
+
+Definition paire_entiers := (nat * nat)%type.
+Definition nuplet := (nat * bool) %type.
+
+Definition plateau : nuplet := (9,true).
